a554033e15907de22796577384f15406661b76dc
Eric Rocha <ericrochaeric@gmail.com>
2021-05-13 23:35:23 -0300

    init

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Input, Text } from 'react-native-elements';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text h3>Entre no TemTudoAki</Text>
+      <Input
+        placeholder="E-mail"
+        leftIcon={{ type: 'font-awesome', name: 'envelope' }}
+        keyboardType="email-address"
+      />
     </View>
   );
 }
